e2dcae25648b2b659ef9076632abd79fa6bc27ce
gmgm60 <gmgm60@gmgm60s-MacBook-Pro.local>
2022-03-28 19:34:50 +0200

    edit migrate

--- database/migrations/2022_03_17_130402_create_jobs_table.php
+++ database/migrations/2022_03_17_130402_create_jobs_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('JobName');
             $table->string('Description');
-            $table->timestamps('Published_Date');
+            $table->timestamps('PublishedDate');
         });
     }
 
